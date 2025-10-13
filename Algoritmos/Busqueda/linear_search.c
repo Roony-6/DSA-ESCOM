fbdba484b6167258d2a27f080c3b98c6307788c4
Roony-6 <roony7.rrc@gmail.com>
2025-10-13 07:42:20 -0600

    linear search algorithm

--- Algoritmos/Busqueda/linear_search.c
+++ Algoritmos/Busqueda/linear_search.c
@@ -7,7 +7,7 @@ int main(){
     return 0;
 }
 
-int linearSearch(int target, int n, int arr*){
+int linearSearch(int target, int n, int *arr){
     for(int i=0;i<n;i++){
         if (arr[i]== target)
             return i;
